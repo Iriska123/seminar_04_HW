29db37a9babbbce385cfb0ed35331fccd82745c1
Irina <irinapetr1110@mail.ru>
2023-08-08 23:09:38 +0300

    done

---
+++ task_02/Program.cs
@@ -0,0 +1,17 @@
+﻿/*Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12*/
+
+Console.WriteLine(Sum(2222));
+
+int Sum (int n) {
+   
+    string nStr = n.ToString();
+    int sum = 0;
+    
+    for(int i = 0; i < nStr.Length; i++) {
+        sum+= Convert.ToInt32("" + nStr[i]);
+    }
+return sum;
+}
---
+++ task_03/Program.cs
@@ -0,0 +1,22 @@
+﻿/*Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран. 
+(можно указать любой промежуток)
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33]*/
+
+int[] arr = FillArray(8);
+PrintArray(arr);
+
+int[] FillArray(int size){
+    int[] arr = new int[size];
+    for(int i = 0; i < size; i++) {
+        arr[i] = new Random().Next(1,50);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr) {
+    for(int i = 0; i < arr.Length; i++) {
+    Console.Write(arr[i] + " ");
+    }
+    Console.WriteLine();
+}
